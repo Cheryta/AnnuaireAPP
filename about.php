9c503e3cd30618f2c831fd78f44f9f4b4f466260
Cheryta <98745090+Cheryta@users.noreply.github.com>
2022-04-05 16:44:42 +0200

    ajout d'autres éléments du projet

---
+++ about.php
@@ -0,0 +1,99 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <?php
+            include 'menu.php'
+        ?>
+        <div id="texte3">
+            <p>Welcome to our articles directory</p>
+        </div>
+        <div id="atext3">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ea ad velit cum ut enim? Adipisci accusantium officiis illum voluptas repellat deserunt a assumenda earum quas. Magni odit in repellendus?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit fugit adipisci possimus perspiciatis quidem eaque, harum odit ullam, quia cupiditate provident rerum beatae totam? Quia accusantium asperiores quisquam cupiditate consectetur.
+            </p>
+        </div>
+        <div id="texte3">
+            <p>Gallery of our promotional items</p>
+        </div>
+        <section id="gallery" class="gallery">
+            <div class="container-fluid p-2">
+            <div class="container-fluid">
+            <div class="row g-0">
+                <div class="col-lg-3 col-md-4">
+                    <div class="gallery-item">
+                        <a href="../css/image/gallery-1.jpg" class="galelry-lightbox">
+                        <img src="../css/image/gallery-1.jpg" alt="" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                    <a href="../css/image/gallery-2.jpg" class="galelry-lightbox">
+                    <img src="../css/image/gallery-2.jpg" alt="" class="img-fluid">
+                    </a>
+                </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                    <a href="../css/image/gallery-3.jpg" class="galelry-lightbox">
+                    <img src="../css/image/gallery-3.jpg" alt="" class="img-fluid">
+                    </a>
+                </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                    <a href="../css/image/gallery-4.jpg" class="galelry-lightbox">
+                    <img src="../css/image/gallery-4.jpg" alt="" class="img-fluid">
+                    </a>
+                </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                    <a href="../css/image/gallery-5.jpg" class="galelry-lightbox">
+                    <img src="../css/image/gallery-5.jpg" alt="" class="img-fluid">
+                    </a>
+                </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                    <a href="../css/image/gallery-6.jpg" class="galelry-lightbox">
+                    <img src="../css/image/gallery-6.jpg" alt="" class="img-fluid">
+                    </a>
+                </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                    <a href="../css/image/gallery-7.jpg" class="galelry-lightbox">
+                    <img src="../css/image/gallery-7.jpg" alt="" class="img-fluid">
+                    </a>
+                </div>
+                </div>
+
+                <div class="col-lg-3 col-md-4">
+                <div class="gallery-item">
+                    <a href="../css/image/gallery-8.jpg" class="galelry-lightbox">
+                    <img src="../css/image/gallery-8.jpg" alt="" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+          </div>
+       </div>
+     </section>
+
+    
+        <div id="texte2">
+            <p>Copyright@2022|Designed with by CIJ </p>
+        </div>
+    </body>
+</html>
---
+++ home.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div class="home">
+            <div id="container0">
+                <img src="./Images/entre1.jpg" alt="">
+                <div id="atext1">
+                    <p>Annuaire des</p>
+                    <h1>articles</h1>
+                </div>
+            </div>
+            <div id="container">
+                <!-- zone de connexion -->
+            
+                <form action="verification.php" method="POST">
+                    <div id="texte4">
+                        <p>Connexion</p>
+                    </div>
+                    
+                    <label><b>Username</b></label>
+                    <input type="text" placeholder="Enter your username" name="username" required>
+
+                    <label><b>Password</b></label>
+                    <input type="password" placeholder="Enter your password" name="password" required>
+
+                    <input type="submit" id='submit' value='LOGIN' >
+                    <?php
+                    if(isset($_GET['erreur'])){
+                        $err = $_GET['erreur'];
+                        if($err==1 || $err==2)
+                            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                    }
+                    ?>
+                </form>
+            </div>
+        </div>
+        
+        <div id="texte2">
+            <p>Copyright@2022|Designed with by CIJ </p>
+        </div>
+    </body>
+</html>
